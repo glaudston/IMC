92f54e933b98f3cc1dd6738855647303306e2bd7
Glaudston Martins <glaudstonalan@gmail.com>
2022-05-17 21:46:13 -0300

    func.01

--- imc.js
+++ imc.js
@@ -1,9 +1,17 @@
 function calcular(){
     let nome = document.getElementById("nome").value;
     let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
+    let altura = document.getElementById("altura").
+    value;
+    if ( altura =="" || peso =="" ||nome == ""){
+        alert("Digite os valores no campo abaixo")
+        
+    } else {
+     
+  
+    let imc = (Number.parseFloat(peso)/Number.parseFloat((altura*altura))).toFixed(2);
 
-    let imc = Number.parseFloat(peso)/Number.parseFloat((altura)*2)
+    document.getElementById("resultado").innerHTML = `<br>Olá ${nome}, éste é o seu Indice de Massa corporal !<br><br>  <strong>${imc}</strong> . `}
 
-    alert(`olá ${nome}, seu peso é ${imc} `)
+  
 }
